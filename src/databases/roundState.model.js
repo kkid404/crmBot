0342b500f0ddb0eab4f6d723d17ddd4ea80ce9b5
Fjornir <nikita.savchenko1997@gmail.com>
2025-09-16 13:44:53 +0300

    обновил схему

--- src/databases/roundState.model.js
+++ src/databases/roundState.model.js
@@ -1,15 +1,18 @@
-// roundState.model.js
 const mongoose = require('mongoose');
 
 const RoundStateSchema = new mongoose.Schema({
     key: {
         type: String,
         required: true,
-        unique: true // чтобы всегда был один документ на всю очередь
+        unique: true // всегда один документ на всю очередь
     },
     processedBuyers: {
         type: [String], // массив ID покупателей
         default: []
+    },
+    roundStartTime: {
+        type: Date,
+        default: null // чтобы можно было проверить и восстановить при первом запуске
     }
 });
 
